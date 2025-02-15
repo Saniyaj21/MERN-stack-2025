b053f88495721a5e26e44b67847bc5511565621a
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-02-15 11:02:39 +0530

    js code

---
+++ app.js
@@ -0,0 +1,29 @@
+
+// console.log(host)
+
+// var host = 'VERCEL';
+// console.log(host)
+// // global scope
+// const age = 90;
+// const a = 100;
+
+// function demo() {
+//     // local scope
+//     const a = 12;
+//     console.log(age)
+//     console.log(a)
+// }
+// demo()
+
+
+
+
+
+
+
+// console.log(age) // Output: 30
+console.log("Start");
+setTimeout(() => {
+    console.log("Async execution");
+}, 4000);
+console.log("End");
